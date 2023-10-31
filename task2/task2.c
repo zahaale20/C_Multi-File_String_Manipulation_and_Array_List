625fd5c5c16d79d14f481bffd32c8f2cbdb1c881
zahaale20 <alexcz@outlook.com>
2023-10-31 14:22:49 -0700

    task 2: reallocating and adding input to the list

--- task2/task2.c
+++ task2/task2.c
@@ -27,15 +27,33 @@ array_list *array_list_new(size_t cap){
 }
 
 void array_list_add_to_end(array_list * list, const char *input){
+    // list error handling
     if (list == NULL){
         fprintf(stderror, "Error: Locating array.");
         return NULL;
     }
 
+    // input error handling
     if (input == NULL){
         fprintf(stderror, "Error: Locating string to add to end of array list.");
         return NULL;
     }
 
-    
+    // reallocate array to be able to hold the additional element
+    char **temp = (char **)realloc(list->elements, sizeof(char *) * (list->capacity + 1));
+
+    // reallocation error handling
+    if (list == NULL){
+        fprintf(stderror, "Error: Reallocation memory for new array list.");
+        return NULL;
+    }
+
+    // add input to elements array now that the list has been reallocated
+    list->elements = temp;
+    list->elements[list->length] = input;
+
+    // increase capacity and length to reflect the change
+    list->capacity++;
+    list->length++;
+
 }
