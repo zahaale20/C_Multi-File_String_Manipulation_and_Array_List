35d690e024e4b1a0fab1343b22956bcefb825f56
zahaale20 <alexcz@outlook.com>
2023-10-31 13:32:33 -0700

    completed task1

--- task1/task1.c
+++ task1/task1.c
@@ -1,6 +1,21 @@
 #include "task1.h"
 #include <stdio.h>
+#include <ctype.h>
 
-void str_lower(char *input, char buffer){
-    
+// copy and modify characters of an existing string to the output buffer
+void str_lower(char *input, char *output){
+    size_t i;
+    for(i = 0; input[i] != '\0'; i++){
+        output[i] = tolower(input[i]);
+    }
+    output[i] = '\0';
+}
+
+
+// modify characters in place (in the same string)
+void str_lower_mutate(char *input){
+    size_t i;
+    for(i = 0; input[i] != '\0'; i++){
+        input[i] = tolower(input[i]);
+    }
 }
--- task1/task1_tests.c
+++ task1/task1_tests.c
@@ -1,6 +1,7 @@
 #include "checkit.h"
 #include "task1.h"
 
+// Given Test
 void test1() {
    char input[] = "Hello THERE";
    char result[15];
@@ -11,7 +12,30 @@ void test1() {
    checkit_string(result, expected);
 }
 
+// Testing function str_lower
+void test2() {
+   char input[] = "AlEx ZaHaRiA";
+   char result[15];
+   char *expected = "alex zaharia";
+
+   str_lower(input, result);
+
+   checkit_string(result, expected);
+}
+
+// Testing function str_lower_mutate
+void test3() {
+   char input[] = "rOcK CrEEk";
+   char *expected = "rock creek";
+
+   str_lower_mutate(input);
+
+   checkit_string(input, expected);
+}
+
 int main(void) {
    test1();
+   test2();
+   test3();
    return 0;
 }
